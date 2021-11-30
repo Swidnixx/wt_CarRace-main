bffe5be95f140d5b8d10873aa25d8f6a49390e21
swid <me@example.com>
2021-11-30 18:55:52 +0100

    Konfiguracja PUN

--- MultiRaceCoal/Assets/Scripts/CarApperance.cs
+++ MultiRaceCoal/Assets/Scripts/CarApperance.cs
@@ -15,6 +15,22 @@ public class CarApperance : MonoBehaviour
 
 	void Start()
 	{
+		if(playerNumber == 0)
+        {
+			playerName = PlayerPrefs.GetString("PlayerName");
+			carColor = MenuController.IntToColor(
+				PlayerPrefs.GetInt("red"),
+				PlayerPrefs.GetInt("green"),
+				PlayerPrefs.GetInt("blue")
+			);
+        }
+		else
+        {
+			playerName = "Random " + playerNumber;
+			float r = Random.Range(0f, 1f);
+			carColor = new Color(r, r, r);
+        }
+
 		nameText.text = playerName;
 		carRenderer.material.color = carColor;
 		nameText.color = carColor;
--- MultiRaceCoal/Assets/Scripts/MenuController.cs
+++ MultiRaceCoal/Assets/Scripts/MenuController.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Renderer carRenderer;
+
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
+
+    public InputField playerName;
+
+    public void SetName(string name)
+    {
+        PlayerPrefs.SetString("PlayerName", name);
+    }
+
+    public static Color IntToColor(int red, int green, int blue)
+    {
+        float r = (float)red / 255;
+        float g = (float)green / 255;
+        float b = (float)blue / 255;
+
+        return new Color(r, g, b);
+    }
+
+    void SetCarColor(int red, int green, int blue)
+    {
+        Color color = IntToColor(red, green, blue);
+        carRenderer.material.color = color;
+
+        PlayerPrefs.SetInt("red", red);
+        PlayerPrefs.SetInt("green", green);
+        PlayerPrefs.SetInt("blue", blue);
+    }
+
+    public void JoinRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     void Start()
     {
-        
+        redSlider.value = PlayerPrefs.GetInt("red");
+        greenSlider.value = PlayerPrefs.GetInt("green");
+        blueSlider.value = PlayerPrefs.GetInt("blue");
+
+        playerName.text = PlayerPrefs.GetString("PlayerName");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetCarColor((int)redSlider.value, (int)greenSlider.value, (int)blueSlider.value);
     }
 }
